d9beb0889b6b529097d38ac86c3d018eba4caec9
ﾓｯﾁｰｻｧﾝ <67029618+unagimochimochi@users.noreply.github.com>
2020-08-25 10:17:00 +0900

    DateIntervalをプリント
    時間差計算にはDateIntervalではなくCalendarを使う？
    とりあえずコミット。

--- ViewController.swift
+++ ViewController.swift
@@ -8,19 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
     var datePicker: UIDatePicker = UIDatePicker()
+    var estimatedTime = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        textField.delegate = self
+        
         // 0.1秒ごとに処理
-        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
         
         // UIDatePickerの設定
         datePicker.locale = Locale(identifier: "ja_JP")
@@ -48,6 +51,18 @@ class ViewController: UIViewController {
         let now = Date()
         
         currentTimeLabel.text = dateFormatter.string(from: now)
+        
+        if let text = textField.text, !text.isEmpty {
+            // 表示する時刻の設定
+            let dateIntervalF = DateIntervalFormatter()
+            dateIntervalF.locale = Locale(identifier: "ja_JP")
+            dateIntervalF.timeZone = .autoupdatingCurrent
+            dateIntervalF.dateStyle = .full
+            dateIntervalF.timeStyle = .medium
+            
+            let timeInterval = dateIntervalF.string(from: now, to: estimatedTime)
+            print(timeInterval)
+        }
     }
     
     @objc func done() {
@@ -62,6 +77,7 @@ class ViewController: UIViewController {
         dateFormatter.timeStyle = .short
         
         textField.text = dateFormatter.string(from: datePicker.date)
+        estimatedTime = datePicker.date
     }
 
 }
